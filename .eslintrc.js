de92ccee6b929ff7c941041a18e75973fe997919
Degasperi <celso.gasperi@universo.univates.br>
2021-10-02 21:37:29 -0300

    NLW6 class 2: new endpoint for create users

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
